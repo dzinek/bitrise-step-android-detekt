e5a8dcf2e40866c8a87cb5781cf535795d0dee7f
Tomasz Nowodziński <tomasz.nowodzinski@gmail.com>
2018-12-28 12:10:57 +0100

    Added running detekt task in main.go

--- main.go
+++ main.go
@@ -3,29 +3,194 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-io/go-utils/sliceutil"
+	"github.com/bitrise-steplib/bitrise-step-android-unit-test/cache"
+	"github.com/bitrise-tools/go-android/gradle"
+	"github.com/bitrise-tools/go-steputils/stepconf"
+	shellquote "github.com/kballard/go-shellquote"
 )
 
-func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+// Config ...
+type Config struct {
+	ProjectLocation   string `env:"project_location,dir"`
+	ReportPathPattern string `env:"report_path_pattern"`
+	Variant           string `env:"variant"`
+	Module            string `env:"module"`
+	Arguments         string `env:"arguments"`
+	CacheLevel        string `env:"cache_level,opt[none,only_deps,all]"`
+	DeployDir         string `env:"BITRISE_DEPLOY_DIR,dir"`
+}
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+func getArtifacts(gradleProject gradle.Project, started time.Time, pattern string) (artifacts []gradle.Artifact, err error) {
+	artifacts, err = gradleProject.FindArtifacts(started, pattern, true)
 	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
+		return
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
-	os.Exit(0)
+	if len(artifacts) == 0 {
+		if !started.IsZero() {
+			log.Warnf("No artifacts found with pattern: %s that has modification time after: %s", pattern, started)
+			log.Warnf("Retrying without modtime check....")
+			fmt.Println()
+			return getArtifacts(gradleProject, time.Time{}, pattern)
+		}
+		log.Warnf("No artifacts found with pattern: %s without modtime check", pattern)
+		log.Warnf("If you have changed default report export path in your gradle files then you might need to change ReportPathPattern accordingly.")
+	}
+	return
+}
+
+func filterVariants(module, variant string, variantsMap gradle.Variants) (gradle.Variants, error) {
+	// if module set: drop all the other modules
+	if module != "" {
+		v, ok := variantsMap[module]
+		if !ok {
+			return nil, fmt.Errorf("module not found: %s", module)
+		}
+		variantsMap = gradle.Variants{module: v}
+	}
+	// if variant not set: use all variants
+	if variant == "" {
+		return variantsMap, nil
+	}
+	filterVariants := gradle.Variants{}
+	for m, variants := range variantsMap {
+		for _, v := range variants {
+			if strings.ToLower(v) == strings.ToLower(variant) {
+				filterVariants[m] = append(filterVariants[m], v)
+			}
+		}
+	}
+	if len(filterVariants) == 0 {
+		return nil, fmt.Errorf("variant: %s not found in any module", variant)
+	}
+
+	return filterVariants, nil
+}
+
+func failf(f string, args ...interface{}) {
+	log.Errorf(f, args...)
+	os.Exit(1)
+}
+
+func runDetektTask(config Config) error {
+	gradleProject, err := gradle.NewProject(config.ProjectLocation)
+	if err != nil {
+		return fmt.Errorf("Failed to open project, error: %s", err)
+	}
+
+	detektTask := gradleProject.GetTask("detekt")
+
+	log.Infof("Variants:")
+	fmt.Println()
+
+	variants, err := detektTask.GetVariants()
+	if err != nil {
+		return fmt.Errorf("Failed to fetch variants, error: %s", err)
+	}
+
+	filterVariants, err := filterVariants(config.Module, config.Variant, variants)
+	if err != nil {
+		failf("Failed to find buildable variants, error: %s", err)
+	}
+
+	for module, variants := range variants {
+		log.Printf("%s:", module)
+		for _, variant := range variants {
+			if sliceutil.IsStringInSlice(variant, filterVariants[module]) {
+				log.Donef("✓ %s", strings.TrimSuffix(variant, "UnitTest"))
+				continue
+			}
+			log.Printf("- %s", strings.TrimSuffix(variant, "UnitTest"))
+		}
+	}
+	fmt.Println()
+
+	started := time.Now()
+
+	args, err := shellquote.Split(config.Arguments)
+	if err != nil {
+		return fmt.Errorf("Failed to parse arguments, error: %s", err)
+	}
+
+	log.Infof("Run detekt:")
+	detektCommand := detektTask.GetCommand(filterVariants, args...)
+
+	fmt.Println()
+	log.Donef("%s" + detektCommand.PrintableCommandArgs())
+	fmt.Println()
+
+	taskError := detektCommand.Run()
+	if taskError != nil {
+		log.Errorf("Detekt task failed, error: %v", taskError)
+	}
+	fmt.Println()
+
+	log.Infof("Exporting artifacts:")
+	fmt.Println()
+
+	artifacts, err := getArtifacts(gradleProject, started, config.ReportPathPattern)
+	if err != nil {
+		return fmt.Errorf("failed to find artifacts, error: %v", err)
+	}
+
+	if len(artifacts) > 0 {
+		for _, artifact := range artifacts {
+			exists, err := pathutil.IsPathExists(
+				filepath.Join(config.DeployDir, artifact.Name),
+			)
+			if err != nil {
+				return fmt.Errorf("failed to check path, error: %v", err)
+			}
+
+			artifactName := filepath.Base(artifact.Path)
+
+			if exists {
+				timestamp := time.Now().Format("20060102150405")
+				ext := filepath.Ext(artifact.Name)
+				name := strings.TrimSuffix(filepath.Base(artifact.Name), ext)
+				artifact.Name = fmt.Sprintf("%s-%s%s", name, timestamp, ext)
+			}
+
+			log.Printf("  Export [ %s => $BITRISE_DEPLOY_DIR/%s ]", artifactName, artifact.Name)
+
+			if err := artifact.Export(config.DeployDir); err != nil {
+				log.Warnf("failed to export artifacts, error: %v", err)
+			}
+		}
+	} else {
+		log.Warnf("No artifacts found with pattern: %s", config.ReportPathPattern)
+		log.Warnf("If you have changed default report file paths with lintOptions/htmlOutput or lintOptions/xmlOutput")
+		log.Warnf("in your gradle files then you might need to change ReportPathPattern accordingly.")
+	}
+
+	return taskError
+}
+
+func main() {
+	var config Config
+
+	if err := stepconf.Parse(&config); err != nil {
+		failf("Couldn't create step config: %v\n", err)
+	}
+
+	stepconf.Print(config)
+	fmt.Println()
+
+	if err := runDetektTask(config); err != nil {
+		failf("%s", err)
+	}
+
+	fmt.Println()
+	log.Infof("Collecting cache:")
+	if warning := cache.Collect(config.ProjectLocation, cache.Level(config.CacheLevel)); warning != nil {
+		log.Warnf("%s", warning)
+	}
+	log.Donef("  Done")
 }
